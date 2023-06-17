c3193faac1722540426c8ba84c84aeefc6337260
suyeon <ibaesuyeon@Naver.com>
2023-06-17 15:46:13 +0900

    [Chore] colors.js 추가
    색상 코드

---
+++ src/colors.js
@@ -0,0 +1,11 @@
+export const WHITE = '#ffffff';
+export const BLACK = '#000000';
+
+export const PRIMARY = {
+  DEFAULT: '#1176C0',
+  ORANGE: '#FF7A50',
+  BUTTON: '#B9D5E9',
+};
+export const GRAY = {
+  DEFAULT: '#a3a3a3',
+};
