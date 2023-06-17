d6a302ea42700a9415c7675e659fc1c0e4c765aa
suyeon <ibaesuyeon@Naver.com>
2023-06-18 00:55:03 +0900

    [Feat] 로그인 화면 구현
    SignInScreen, Button, Input.js

---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,114 @@
+import {
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
+import { useEffect, useState } from 'react';
+import Button from '../components/Button';
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    setDisabled(!(email.trim() && password.trim()));
+  }, [email, password]);
+
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    console.log(email, password);
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.select({ ios: 'padding' })}
+    >
+      <Pressable style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
+        <View style={styles.container}>
+          <Image
+            source={require('../../assets/logo.png')}
+            style={styles.image}
+          />
+          <Input
+            title={'이메일'}
+            placeholder="your@email.com"
+            keyboardType={KeyboardTypes.EMAIL}
+            returnKeyType={ReturnKeyTypes.NEXT}
+            value={email}
+            onChangeText={(email) => setEmail(email.trim())}
+          />
+          <Input
+            title={'비밀번호'}
+            returnKeyType={ReturnKeyTypes.DONE}
+            secureTextEntry
+            value={password}
+            onChangeText={(password) => setPassword(password.trim())}
+            onSubmitEditing={onSubmit}
+          />
+          <View style={styles.buttonContainer}>
+            <Button
+              title={'로그인'}
+              onPress={onSubmit}
+              disabled={disabled}
+              // onPress={() => navigation.navigate(AuthRoutes.CALENDAR_View)}
+            />
+          </View>
+          <View style={styles.buttonWrapper}>
+            <Text
+              style={styles.textButton}
+              onPress={() => console.log('비밀번호 찾기 화면으로 전환')}
+              // navigation.navigate(AuthRoutes.Account)}
+            >
+              비밀번호 찾기
+            </Text>
+            <Text
+              style={styles.textButton}
+              onPress={() => console.log('회원가입 화면으로 전환')}
+              // navigation.navigate(AuthRoutes.Account)}
+            >
+              회원가입
+            </Text>
+          </View>
+        </View>
+      </Pressable>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 100,
+    marginBottom: 30,
+  },
+  buttonContainer: {
+    width: '100%',
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  buttonWrapper: {
+    marginTop: 30,
+    flexDirection: 'row',
+  },
+  textButton: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginHorizontal: 30,
+    justifyContent: 'space-evenly',
+  },
+});
+
+export default SignInScreen;
