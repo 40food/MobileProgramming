7f19367d076c743e51eb3d634db3a8126a7b320f
KimHyeongKeun <107840065+KimHyeongKeun@users.noreply.github.com>
2023-06-19 02:31:27 +0900

    [Fix]ToDo화면수정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import SignInScreen from './screens/SignInScreen';
@@ -5,16 +6,25 @@ import SignUpScreen from './screens/SignUpScreen';
 import Logo from './components/Logo';
 import Navigation from './navigations/Navigation';
 import SettingScreen from './screens/SettingScreen';
+=======
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+>>>>>>> 410b0f6 (first)
 
 export default function App() {
   return (
     <View style={styles.container}>
+<<<<<<< HEAD
       <StatusBar style="auto" />
       {/* <SignUpScreen /> */}
       {/* <StatusBar style="dark" /> */}
       <Navigation>
         <SignInScreen />
       </Navigation>
+=======
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+>>>>>>> 410b0f6 (first)
     </View>
   );
 }
@@ -22,9 +32,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+<<<<<<< HEAD
     backgroundColor: '#fff',
     // alignItems: 'center',
     // justifyContent: 'center',
     width: '100%',
+=======
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+>>>>>>> 410b0f6 (first)
   },
 });
